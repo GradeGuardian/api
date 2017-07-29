8b3048299c0f39fa5f0a63025c3ff6a143d9c1b0
Andrew Arpasi <andrew@andrewarpasi.com>
2017-07-28 21:40:51 -0400

    Setup express server

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+
+var api = express.Router();
+app.use('/api/v1', api);
+app.use(cors());
+
+module.exports.app = app;
+module.exports.api = api;
+
+app.get('/', (req, res) => {
+    res.send('GradeGuardian');
+});
+
+const port = 4404;
+
+const server = app.listen(port, () => {
+    console.log("Running on port " + port);
+});
