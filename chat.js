4e9c506aa7fe6f74d651c4e0d97ce35a68881c90
Andrew Arpasi <andrew@andrewarpasi.com>
2017-07-30 21:16:38 -0400

    Chat session management and start data collect

--- chat.js
+++ chat.js
@@ -1,12 +1,13 @@
 const application = require('./index')
 const secrets = require('./secrets')
 const apiai = require('apiai')
+const randomstring = require('randomstring')
 
 const server = application.server
 
 const io = require('socket.io').listen(server)
 
-let socketListeners = {}
+let socketListeners = []
 
 io.on('connection', (socket) => {
     console.log('User connected')
@@ -14,6 +15,9 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('User disconnected')
     })
+
+    const session = newSession(socket)
+    socketListeners.push(session)
 
     socket.on('message', (message) => {
         console.log('User: ', message.message)
@@ -25,7 +29,14 @@ io.on('connection', (socket) => {
         })
     })
     
-    socket.emit('message', newMessage('Hi, I\'m Desiree. I\'m built to get you better grades. Ready to get started?'))
+    socket.emit('message', newMessage('Hi, I\'m Desiree. I\'m built to get you better grades. I\'m going to be asking some questions so please bear with me.'))
+    setTimeout(function(){
+        sendToAPI('start-data-collect',function(data) {
+            const response = data.result.fulfillment.speech
+            console.log('Desiree: ' + response)
+            socket.emit('message',newMessage(response))
+        })
+    },1000)
 })
 
 function newMessage(text) {
@@ -36,11 +47,18 @@ function newMessage(text) {
     }
 }
 
+function newSession(socket) {
+    return {
+        sessionId: randomstring.generate(16),
+        dbObjectId: null,
+    }
+}
+
 const desiree = apiai(secrets.apiai_token)
 
-function sendToAPI(text,callback) {
+function sendToAPI(text,session,callback) {
     var request = desiree.textRequest(text, {
-        sessionId: 'test123'
+        sessionId: session.sessionId,
     })
     request.on('response', function(response) {
         callback(response)
