d205b219fc8d801814eaf289fd771cba97fe6a9d
Andrew Arpasi <andrew@andrewarpasi.com>
2017-08-07 15:43:04 -0400

    Messages for finished data collection

--- chatstate.js
+++ chatstate.js
@@ -11,6 +11,16 @@ function handleResponse(response,session) {
     })
     return true
   }
+  else if(message === 'finish-collect') {
+    session.socket.emit('message',chat.newMessage('Thanks you for helping us help you help us all! That\'s all the questions I have today.'));
+    setTimeout(function() {
+      session.socket.emit('message',chat.newMessage('Your info has been sent to the NSA (Node Server Application) and will be used by your advisor to help you improve your grades.'))
+    },1000)
+    setTimeout(function() {
+      session.socket.emit('message',chat.newMessage('Now that we are done, feel free to make small talk with me!'))
+    },2000)
+    return true;
+  }
   return false
 }
 
