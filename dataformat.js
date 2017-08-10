efe188c28531dd248de46418dde039e637b733db
Andrew Arpasi <andrew@andrewarpasi.com>
2017-08-10 00:25:59 -0400

    Method to format data into readable format

--- dataformat.js
+++ dataformat.js
@@ -51,7 +51,40 @@ function formatListForAdvisor(rawStudents) {
 }
 
 function formatStudentForAdvisor(rawStudent) {
+  const student = {
+    Name: rawStudent.name,
+    atRisk: rawStudent.atRisk,
+    GPA: (parseInt(rawStudent.G1) + parseInt(rawStudent.G2)) / 10,
+    Age: rawStudent.age,
+  }
+  student['Living Status'] = rawStudent.Pstatus == 'A' ? 'Apart' : 'Together'
+  student['Mother\'s Education'] = ['None','Primary Education','Secondary Education','5th to 9th grade','Higher Education'][parseInt(rawStudent.Medu)]
+  student['Father\'s Education'] = ['None','Primary Education','Secondary Education','5th to 9th grade','Higher Education'][parseInt(rawStudent.Medu)]
+  student['Mother\'s Job'] = rawStudent.Mjob
+  student['Father\'s Job'] = rawStudent.Fjob
+  student['Reason for school choice'] = rawStudent.reason
+  student['Travel Time'] = ['',',<15 min','15 to 30 min','30 min to 1 hour','> 1 hour'][parseInt(rawStudent.traveltime)]
+  student['Hours Spent Studying per Week'] = ['',',<2 hr','2 to 5 hrs','5 to 10 hrs','> 10 hrs'][parseInt(rawStudent.studytime)]
+  student['Class Failures'] = rawStudent.failures
+  student['Extra Educational Support'] = yesNo(rawStudent.schoolsup)
+  student['Family Educational Support'] = yesNo(rawStudent.famsup)
+  student['Extra Paid Classes?'] = yesNo(rawStudent.paid)
+  student['Extra Curriculars?'] = yesNo(rawStudent.activities)
+  student['Attends Nursery School?'] = yesNo(rawStudent.nursery)
+  student['Has Internet?'] = yesNo(rawStudent.internet)
+  student['In a relationship?'] = yesNo(rawStudent.romantic)
+  student['Quality of Family Relationships (numeric: from 1 - very bad to 5 - excellent)'] = rawStudent.famrel
+  student['Free time after school (numeric: from 1 - very low to 5 - very high)'] = rawStudent.freetime
+  student['Going out with friends (numeric: from 1 - very low to 5 - very high)'] = rawStudent.goout
+  student['Workday alcohol consumption (numeric: from 1 - very low to 5 - very high)'] = rawStudent.Dalc
+  student['Weekend alcohol consumption (numeric: from 1 - very low to 5 - very high)'] = rawStudent.Walc
+  student['Current health status (numeric: from 1 - very bad to 5 - very good)'] = rawStudent.health
+  student['Number of school absences'] = rawStudent.absences
+  return student
+}
 
+function yesNo(value) {
+  return value == '1' ? 'Yes' : 'No'
 }
 
 module.exports.formatDataForMLAndSave = formatDataForMLAndSave
