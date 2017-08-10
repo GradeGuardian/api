fbdd4c5e95c3804c28fca06eb6bc297738de9c86
Andrew Arpasi <andrew@andrewarpasi.com>
2017-08-09 22:48:59 -0400

    Fix typo

--- predict.js
+++ predict.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 function predictWithFormattedData(data) {
   request.post('http://159.203.94.60/',{json: data},function(err,res,body) {
-    if(!error && response.statusCode == 200) {
+    if(!err && res.statusCode == 200) {
       console.log(body)
     } else {
       console.log("Error: ",err)
